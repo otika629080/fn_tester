075bd10959b5fe046ee75f4ee0002a2480872a58
nag07 <nagastec.sc07@gmail.com>
2025-01-16 13:54:33 +0900

    eslintの設定変更

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    eslint: {
+    ignoreDuringBuilds: true, // ビルド中にESLintエラーを無視する
+  },
 };
 
 export default nextConfig;
